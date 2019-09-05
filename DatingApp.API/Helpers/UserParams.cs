df8b8af1d337ca17b456fe86171a1ab4f731cff3
Shahir1630 <16303019@iubat.edu>
2019-09-05 23:16:10 +0600

    Endd of Section 15

--- DatingApp.API/Helpers/UserParams.cs
+++ DatingApp.API/Helpers/UserParams.cs
@@ -16,7 +16,10 @@ namespace DatingApp.API.Helpers
         public int MinAge { get; set; } = 18;
         public int MaxAge { get; set; } = 99; 
 
-        public string OrderBy { get; set; }     
+        public string OrderBy { get; set; }
+        public bool Likees { get; set; } = false;
+        public bool Likers { get; set; } = false;
+
 
     }
 }
